ab9fd5b174b7222d757d2c84cc98ff1b89412a24
Katsuyuki Nakagawaji <nakagawaji@arte-inc.co.jp>
2016-06-05 00:54:53 +0900

    singletonをenumに変更

--- source/creational/singleton.swift
+++ source/creational/singleton.swift
@@ -8,14 +8,10 @@ There are very few applications, do not overuse this pattern!
 
 ### Example:
 */
-class DeathStarSuperlaser {
-    static let sharedInstance = DeathStarSuperlaser()
-
-    private init() {
-        // Private initialization to ensure just one instance is created.
-    }
+enum Singleton {
+    case INSTANCE
 }
 /*:
 ### Usage:
 */
-let laser = DeathStarSuperlaser.sharedInstance
+let singleton = Singleton.INSTANCE;
